7207928e3b60128c6183c47643d8a34595ac7b18
Mari <marinarosemartin@gmail.com>
2020-09-19 16:18:24 -0500

    cleaned up apiRoutes code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,8 @@
+// DEPENDENCIES
 var fs = require("fs");
 const path = require("path");
 
+// UUID for adding ID to each note
 const { v4: uuidv4 } = require('uuid');
 
 
@@ -8,10 +10,7 @@ const { v4: uuidv4 } = require('uuid');
 // ROUTING
 module.exports = function (app) {
     app.get("/api/notes", function (req, res) {
-        console.log("here");
-
         fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function (error, data) {
-
             if (error) {
                 return console.log(error);
             }
@@ -25,47 +24,31 @@ module.exports = function (app) {
 
 
 
-    // API POST Requests
-
+    // API POST Requests - adds notes to db.json
     app.post("/api/notes", function (req, res) {
-        // 
-        // console.log(req.body);
-
-
         fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function (error, data) {
-
             if (error) {
                 return console.log(error);
             }
 
-
             const parsedData = JSON.parse(data);
 
+            // adds unique id to each note
             req.body.id = uuidv4();
 
             parsedData.push(req.body);
 
-
             fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(parsedData), function (err) {
-
                 if (err) {
                     return console.log(err);
                 }
 
-                // console.log("Success!");
-
-                // res.json(parsedData);
-
-                // console.log(__dirname);
                 res.sendFile(path.join(__dirname, "../public/notes.html"));
             });
-
         });
-
-
-
     });
 
+    // API DELETE Requests - deletes notes from db.json
     app.delete("/api/notes/:id", function (req, res) {
         console.log(req.params.id);
         fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function (error, data) {
@@ -74,26 +57,17 @@ module.exports = function (app) {
             }
 
             const parsedData = JSON.parse(data);
+
+            // compares selected note id to ids in db.json
             const filteredNotes = parsedData.filter(function (item) {
-                // if (item.id === req.params.id) {
-                //     return false;
-                // }
-                // return true;
                 return item.id !== req.params.id;
             })
-            console.log(filteredNotes);
 
             fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(filteredNotes), function (err) {
-
                 if (err) {
                     return console.log(err);
                 }
 
-                // console.log("Success!");
-
-                // res.json(parsedData);
-
-                // console.log(__dirname);
                 res.sendFile(path.join(__dirname, "../public/notes.html"));
             });
         })
